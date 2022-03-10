7410e3ffdb812ed44e68bd65116a824b24b05b30
Leg0evan <99287638+Leg0evan@users.noreply.github.com>
2022-03-10 10:42:04 -0700

    Update Starter_while.ino

--- Starter_while.ino
+++ Starter_while.ino
@@ -5,23 +5,28 @@ lights. */
 // Initialize variables
 
 int x = 1;
+int pinNum=9;
+int pin1=9;
+int pin2=8;
+int pin3=7;
 
 void setup()
 {
     // Start Serial Moniter and set up pins
     Serial.begin(9600);
-    pinMode ();
-    pinMode ();
-    pinMode ();
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2,OUTPUT);
+    pinMode(pin3, OUTPUT);
     
-    while (x < ) {
+    while (x < 4) {
         // Display the cycle number in the Serial Monitor
         Serial.print("Cycle no ");
         Serial.println(x);
         
         // Turn on each LED in sequence
-        digitalWrite ((x + 7), );
-        delay (2000)
+        digitalWrite(pinNum, HIGH);
+        pinNum=pinNum-1;
+        delay(2000);
         x = x + 1;
     }
 }
