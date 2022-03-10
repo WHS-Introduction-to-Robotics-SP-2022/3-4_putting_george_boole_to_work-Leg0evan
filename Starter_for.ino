302bbd10b3e77328b95148b556b61e5b1c12c57f
Leg0evan <99287638+Leg0evan@users.noreply.github.com>
2022-03-10 09:32:08 -0700

    Update Starter_for.ino

--- Starter_for.ino
+++ Starter_for.ino
@@ -2,23 +2,25 @@
 from completely off to completely on over the course of two seconds*/
 
 // Initialize variables
+int Time=4.25;
+int pin=10;
 int y;
 
 void setup()
 {
     // Set up the LED pin
-    pinMode (6,);
+    pinMode(pin,OUTPUT);
     
     // Fade LED from 0 to full power over one second
-    for (y = 0; y < 256; ){
-        analogWrite (, y);
-        delay ();
+    for (y = 0; y < 256; y=y+=1){
+        analogWrite(pin, y);
+        delay(Time);
     }
     
     // Fade LED from full power to 0 over one second
     for (y = 255; y >= 0; y = y - 1){
-        analogWrite (, y);
-        delay ();
+        analogWrite(pin, y);
+        delay(Time);
     }
 
 }
